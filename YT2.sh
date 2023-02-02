7c849b8d2ee4a47fa336f6d214d44de9707ea90b
HACKER <76677018+HackerTHINS@users.noreply.github.com>
2023-02-02 15:28:14 +0700

    Add files via upload

---
+++ YT2.sh
@@ -0,0 +1,4 @@
+defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[31m\\\]丫ㄖㄩ〸ㄩ⻏🝗 卄闩⼕长\\\[\\\e[0m\\\]\\\[\\\e[39m\\\]by henz\\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[39m\\\]'"
+sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "Released in February 19 2023"
+sleep 10
